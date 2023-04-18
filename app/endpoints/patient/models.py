0514392c53f55d1147a60ca539e10e7ffec79457
Mehdi <sadour.mehdi@gmail.com>
2023-04-19 01:58:09 +0200

    Add data for populate db.

--- app/endpoints/patient/models.py
+++ app/endpoints/patient/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 
 
 class Patient(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="patient_user"
+    )
 
     USERNAME_FIELD = "email"
--- app/endpoints/surgeon/models.py
+++ app/endpoints/surgeon/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.conf import settings
 
 
-class Patient(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+class Surgeon(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="surgeon_user"
+    )
 
     USERNAME_FIELD = "email"
