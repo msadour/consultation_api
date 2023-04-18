c49a9cafe5a563baedfb762e56f0e66b156c8569
Mehdi <sadour.mehdi@gmail.com>
2023-04-19 01:29:20 +0200

    Add Surgeon model.

--- app/endpoints/surgeon/models.py
+++ app/endpoints/surgeon/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Patient(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    USERNAME_FIELD = "email"
